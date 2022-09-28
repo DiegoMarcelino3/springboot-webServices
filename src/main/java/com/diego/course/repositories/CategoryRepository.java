f465f2fd0e091e9d0986c66781d5aa2206b01906
DiegoMarcelino3 <da200958@alunos.unisanta.br>
2022-09-28 18:10:02 -0300

    Category

---
+++ src/main/java/com/diego/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.diego.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.diego.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
